043cc72b8c1162ea876df669fde3142bbef51bfd
Vincent Landau <vincent@csp-inc.org>
2019-06-03 17:27:37 -0600

    updated juno run script

--- run-juno.sh
+++ run-juno.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 xhost local:root
 docker run -d --rm -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-              	   -v $HOME/.atom:/home/atom/.atom \
+              	   -v $HOME/.atom:/root/.atom \
               	   -v "$(pwd)":/home/omniscape \
               	   -e DISPLAY \
               	   vlandau/juno-ide:latest
