235fa2c266e87d2077ebf8122ce221a469f91d01
Vincent Landau <vincent@csp-inc.org>
2019-06-11 16:25:31 -0600

    updated clip to use comprehension instead of nested loop

--- code/clip.jl
+++ code/clip.jl
@@ -2,11 +2,11 @@ clip = function(A::Array{Real,2}, x::Real, y::Real, distance::Real)
     dim1 = size(A)[1]
     dim2 = size(A)[2]
 
-    dist = zeros(dim1, dim2)
-    for i=1:dim1,j=1:dim2
-        dist[i, j] = sqrt((i - x)^2 + (j - y)^2)
-    end
+    dist = [sqrt((i - x)^2 + (j - y)^2) for i = 1:dim1, j = 1:dim2]
+
     clipped = A
-    clipped[dist.>distance] .= -9
-    reshape(clipped, dim1, dim2)
+    clipped[dist .> distance] .= -9999
+
+    output = reshape(clipped, dim1, dim2)
+    output
 end
