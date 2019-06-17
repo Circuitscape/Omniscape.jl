f05e2f2a2cdb256bde5023061108c5cc913eab92
Vincent Landau <vincent@csp-inc.org>
2019-06-17 15:36:15 -0600

    updated run file to use atom image with Circuitscape

--- run-juno.sh
+++ run-juno.sh
@@ -4,4 +4,4 @@ docker run -i --rm --ipc "host" -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
               	   -v $HOME/.atom:/root/.atom \
               	   -v "$(pwd)":/home/omniscape \
               	   -e DISPLAY \
-              	   vlandau/juno-ide:latest
+              	   omniscape-atom-env:latest
