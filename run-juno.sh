280378e4ec6e57b7b53b875fd0a2eb867bd3e77a
Vincent Landau <vincent@csp-inc.org>
2019-06-18 12:17:54 -0600

    updated juno to run in background

--- run-juno.sh
+++ run-juno.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 xhost local:root
-docker run -i --rm --ipc "host" -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+docker run -i -d --rm --ipc "host" -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
               	   -v $HOME/.atom:/root/.atom \
               	   -v "$(pwd)":/home/omniscape \
               	   -e DISPLAY \
