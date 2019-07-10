87b765368fe291792d9243786b26e8b7bc00265c
Vincent Landau <vincent@csp-inc.org>
2019-07-10 12:48:03 -0600

    first pass at make.jl for docs

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+using Documenter, Omniscape
+
+makedocs(
+    modules = [Omniscape],
+    authors = "Vincent A. Landau"
+    sitename = "Omniscape.jl",
+    pages = Any["index.md"]
+)
+
+deploydocs(
+    repo = "github.com/Circuitscape/Omniscape.jl.git",
+    target = "build",
+)
