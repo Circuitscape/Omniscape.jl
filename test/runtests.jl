2bae892f78f777f49fca39f76a554e2f5276a7c8
Vincent Landau <vincent@csp-inc.org>
2019-12-12 11:08:51 -0700

    switch some tests to serial given travis bug. add unit test for matching

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,7 @@
 using Test, Omniscape
+import Omniscape
 
+### Syntax tests for run_omniscape()
 f = run_omniscape("input/config4.ini")
 g = run_omniscape("input/config5.ini")
 a, b, c = run_omniscape("input/config.ini")
@@ -20,3 +22,55 @@ rm("test5_output", recursive = true)
 @test typeof(d) == Array{Float64,2}
 @test typeof(e) == Array{Float64,2}
 @test b ≈ d
+
+### Unit tests for components
+## source target matching
+source_subset = [1.0 1 1 1 1 1 1 1 1 1; # 6x10 array
+                   1 1 1 1 1 1 1 1 1 1;
+                   1 1 1 1 1 1 1 1 1 1;
+                   1 1 1 1 1 1 1 1 1 1;
+                   1 1 1 1 1 1 1 1 1 1;
+                   1 1 1 1 1 1 1 1 1 1]
+
+con1pres =   [1.0 4 3 2 7 6 9 2 3 0;
+                4 2 6 5 3 9 4 5 3 3;
+                1 1 5 2 7 7 8 8 9 1;
+                8 7 6 5 5 5 0 0 1 1;
+                3 5 4 4 4 8 5 0 1 8;
+                1 2 8 2 1 6 5 5 8 7]
+
+con1fut =   [2.0 1 3 2 7 6 7 1 3 0;
+               4 2 4 5 3 9 4 5 5 3;
+               3 8 3 2 6 3 8 5 9 1;
+               8 7 6 5 1 5 0 0 1 1;
+               3 1 4 5 4 8 5 7 1 8;
+               5 2 3 2 1 6 4 5 3 7]
+x = 5
+y = 4
+con1_lower = -1.0
+con1_upper = 1.0
+Omniscape.source_target_match!(source_subset,
+                     1,
+                     con1pres,
+                     con1fut,
+                     Array{Float64, 2}(undef, 1, 1),
+                     Array{Float64, 2}(undef, 1, 1),
+                     "within",
+                     "within",
+                     con1_lower, # lower bound to be within for comparisome
+                     con1_upper,
+                     0.0,
+                     0.0,
+                     y,
+                     y,
+                     x,
+                     x,
+                     1,
+                     6,
+                     1,
+                     10
+                     )
+target_val = con1fut[y, x]
+# Make sure no present day vals in con1 are outside of range
+@test sum((con1pres[source_subset .== 1] .< (target_val + con1_lower)) .|
+    (con1pres[source_subset .== 1] .> (target_val + con1_upper))) == 0
