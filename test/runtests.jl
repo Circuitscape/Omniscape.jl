9130a2ee8a8cecf97c39953d8430e69e6458a24d
Vincent Landau <vincent@csp-inc.org>
2020-01-21 14:44:27 -0700

    remove all test but the one failing for debugging

--- test/runtests.jl
+++ test/runtests.jl
@@ -79,28 +79,28 @@ block_sources = sources_raw[Int(targets[1,2] - int_arguments["block_radius"]):In
 @info "target tests passed"
 
 ### Syntax tests for run_omniscape()
-l, f, p = run_omniscape("input/config4.ini")
+# l, f, p = run_omniscape("input/config4.ini")
 g = run_omniscape("input/config5.ini")
-h = run_omniscape("input/config6.ini")
-a, b, c = run_omniscape("input/config.ini")
-q, e, m = run_omniscape("input/config3.ini")
-d = run_omniscape("input/config2.ini")
+# h = run_omniscape("input/config6.ini")
+# a, b, c = run_omniscape("input/config.ini")
+# q, e, m = run_omniscape("input/config3.ini")
+# d = run_omniscape("input/config2.ini")
 
-rm("test1_output", recursive = true)
-rm("test2_output", recursive = true)
-rm("test3_output", recursive = true)
-rm("test4_output", recursive = true)
+# rm("test1_output", recursive = true)
+# rm("test2_output", recursive = true)
+# rm("test3_output", recursive = true)
+# rm("test4_output", recursive = true)
 rm("test5_output", recursive = true)
-rm("test6_output", recursive = true)
+# rm("test6_output", recursive = true)
 
-@test typeof(f) == Array{Float64,2}
+# @test typeof(f) == Array{Float64,2}
 @test typeof(g) == Array{Float64,2}
-@test typeof(h) == Array{Float64,2}
-@test typeof(a) == Array{Float64,2}
-@test typeof(b) == Array{Float64,2}
-@test typeof(c) == Array{Float64,2}
-@test typeof(d) == Array{Float64,2}
-@test typeof(e) == Array{Float64,2}
-@test b ≈ d #parallel and serial produce same result
+# @test typeof(h) == Array{Float64,2}
+# @test typeof(a) == Array{Float64,2}
+# @test typeof(b) == Array{Float64,2}
+# @test typeof(c) == Array{Float64,2}
+# @test typeof(d) == Array{Float64,2}
+# @test typeof(e) == Array{Float64,2}
+# @test b ≈ d #parallel and serial produce same result
 
 @info "run_omniscape tests passed"
