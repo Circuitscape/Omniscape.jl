ad075dfb45ad17e7371b230a0383eb0400ce9d9b
Vincent Landau <vincent@csp-inc.org>
2020-03-10 12:15:39 -0600

    updated gitignore

--- run-juno.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-xhost local:root
-docker run -i -d --rm --ipc "host" -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-              	   -v $HOME/.atom:/root/.atom \
-              	   -v "$(pwd)":/home/omniscape \
-              	   -e DISPLAY \
-              	   omniscape-dev:latest
