9b788de9a5c0b1bedc86cc7664bdb2b23b3faa98
Vincent Landau <vincent@csp-inc.org>
2020-09-11 13:57:20 -0600

    testin out images for block_size

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,11 @@ using Documenter, Omniscape
 
 const formats = Any[
     Documenter.HTML(
-    	assets = ["assets/custom.css"],
+    	assets = [
+    		"assets/custom.css",
+    		"figs/sources_block_of_1.png",
+    		"figs/sources_block_of_3.png"
+    	],
 	edit_branch = "main",
     ),
 ]
